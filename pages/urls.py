0b608acff420ad4a9c0ef4b56d6bc9cc94da894b
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2021-03-28 13:50:33 -0300

    delete and update

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='foods'),
     path('prato/<slug:slug_text>', views.listing, name='listing'), #<int:listing_id> <slug:slug_text> name='listing'
+    path('<slug:slug_text>/update/', food_update_view, name='food-update'),
+    path('<slug:slug_text>/delete/', food_delete_view, name='food-delete'),
     path('pesquisa', views.search, name='search'),
 ]
